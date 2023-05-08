8742b9bf73e26fb7efd0ef40a77de01a5319523d
CarlosCedeira <cedeira05@gmail.com>
2023-05-08 22:13:38 +0200

    Nos falta corregir el username

---
+++ Project_2/controllers/users/newUser.js
@@ -0,0 +1,25 @@
+const { generateError } = require("../../generateError");
+const { queryNewUser } = require("../../db/createUser");
+
+const newUserControler = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    // Añadir npm JOI para validar email y password
+    if (!email || !password) {
+      throw generateError("Email or password invalid", 404);
+    }
+
+    const id = await queryNewUser(email, password);
+
+    res.send({
+      status: "ok",
+      message: `User created with id: ${id}`,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  newUserControler,
+};
